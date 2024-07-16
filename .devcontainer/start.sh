c3d7127a5ca47fcf6d0968a09906fa1751dda44c
harrisDLEng <harris47naeem@gmail.com>
2024-07-16 20:07:38 +0000

    fix: change the start.sh file

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 
-VENV_PATH=$(poetry env info --path)
-if [[ ! -d $VENV_PATH ]]; then
-  poetry config virtualenvs.in-project true --local
-fi
+# VENV_PATH=$(poetry env info --path)
+# if [[ ! -d $VENV_PATH ]]; then
+#   poetry config virtualenvs.in-project true --local
+# fi
 
 
-if [ ! -d "YOLOX" ]; then
-    git clone --depth=1 https://github.com/Megvii-BaseDetection/YOLOX.git
-fi
+# if [ ! -d "YOLOX" ]; then
+#     git clone --depth=1 https://github.com/Megvii-BaseDetection/YOLOX.git
+# fi
 
-poetry install
-poetry shell
+# poetry install
+# poetry shell
