8ba96fa690f28a55823a3c5d39c7fc2f02ea1960
harrisDLEng <harris47naeem@gmail.com>
2024-07-18 15:37:45 +0000

    fix: change the shebang from  #!/usr/bin/env  -->

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,9 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
+    source "$HOME/anaconda3/etc/profile.d/conda.sh"
+elif [ -f "/opt/conda/etc/profile.d/conda.sh" ]; then
+    source "/opt/conda/etc/profile.d/conda.sh"
+else
+    echo "Conda initialization script not found. Please check your conda installation."
+    exit 1
+fi
 
 if ! conda env list | grep -q 'yolox-aws-lambda'; then
     conda env create --file environment.yml
 fi
 
-source activate base
+conda deactivate
 conda activate yolox-aws-lambda
+
 
