6f4c9a65d0bd0d8312bca5251e0d6841d3996870
harrisDLEng <harris47naeem@gmail.com>
2024-11-12 12:45:44 +0000

    add: tests for predictor and yolox clases

---
+++ tests/test_predictor.py
@@ -0,0 +1,12 @@
+from app.exps.yolox_s import Exp
+from app.predictor.predictor import yolox
+
+
+def test_predictor():
+    pass
+
+
+def test_yolox():
+    model = yolox(Exp())
+
+    assert False
